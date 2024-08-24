50ff369ee977ad60d15c5d308f4adbff54a1e5eb
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-08-24 21:06:05 +0545

    setting home page and shadcn-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../@/components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
