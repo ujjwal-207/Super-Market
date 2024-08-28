1391ab41de6accfbd262e2dab24b8b2b42246052
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-08-28 21:23:37 +0545

    setting backend

--- frontend/src/assets/data.tsx
+++ frontend/src/assets/data.tsx
@@ -1,3 +1,9 @@
+import kurtha from "./kurtha.jpeg";
+import milk from "./milk.jpeg";
+import rice from "./rice.jpeg";
+import tv from "./tv.jpg";
+import toys from "./toys.webp";
+
 const all_product = [
   {
     id: 1,
@@ -5,6 +11,7 @@ const all_product = [
     category: "Clothes",
     new_price: 5000,
     old_price: 7000,
+    image: kurtha,
   },
   {
     id: 2,
@@ -12,6 +19,7 @@ const all_product = [
     category: "Electronics",
     new_price: 5000,
     old_price: 7000,
+    image: tv,
   },
   {
     id: 3,
@@ -19,6 +27,7 @@ const all_product = [
     category: "Diary Product",
     new_price: 5000,
     old_price: 7000,
+    image: milk,
   },
   {
     id: 4,
@@ -26,6 +35,7 @@ const all_product = [
     category: "Groceries",
     new_price: 5000,
     old_price: 7000,
+    image: rice,
   },
   {
     id: 5,
@@ -33,5 +43,6 @@ const all_product = [
     category: "Remote Control Car",
     new_price: 5000,
     old_price: 7000,
+    image: toys,
   },
 ];
